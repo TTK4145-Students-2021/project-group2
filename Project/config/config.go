cbbdea30a8a3d037aaea7b68a4f718bfe1ddcc96
jorgenmyrvold <jorgeum@stud.ntnu.no>
2021-04-11 17:59:11 +0200

    Somewhat woring network module

--- Project/config/config.go
+++ Project/config/config.go
@@ -3,25 +3,25 @@ package config
 import (
 	"os"
 	"strconv"
-
-
+	"time"
 )
 
 //Package for common variables to be defined across the application
 var Port string
+
 var ID int = 1 //the order module is written so that this is the first elevator. (i.e it has -1 in all indexes)
 const NumFloors int = 4
 
 const NumElevators int = 3
 
-//var HallOrder = make(chan elevio.ButtonEvent)  //*****Commented out beacuse of include problems. Possible Fix: intialize in a higher order file. 
-//var ReceivedElevatorUpdate = make(chan orders.ElevatorStatus)  //*****Commented out beacuse of include problems. Possible Fix: intialize in a higher order file. 
+//var HallOrder = make(chan elevio.ButtonEvent)  //*****Commented out beacuse of include problems. Possible Fix: intialize in a higher order file.
+//var ReceivedElevatorUpdate = make(chan orders.ElevatorStatus)  //*****Commented out beacuse of include problems. Possible Fix: intialize in a higher order file.
 var NewFloor = make(chan int)
 var isDoorOpen = make(chan bool)
 
-var BcastPort int = 12345
-var PeersPort int = 54321
-
+const BcastPort int = 12345
+const PeersPort int = 54321
+const BcastIntervall time.Duration = 1000 * time.Millisecond
 
 func InitConfig() {
 	Port = os.Args[1]
